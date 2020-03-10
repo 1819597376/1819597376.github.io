e80e1a907888c1b9e851ab6f87c29c78e5e7188f
1819597376 <1819597376@qq.com>
2020-03-11 00:27:37 +0800

    Site updated: 2020-03-11 00:27:30

--- js/main.js
+++ js/main.js
@@ -1,189 +1,71 @@
-(function ($) {
-    $('.article img:not(".not-gallery-item")').each(function () {
-        // wrap images with link and add caption if possible
-        if ($(this).parent('a').length === 0) {
-            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
-            if (this.alt) {
-                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
-            }
-        }
+/*!
+ * Clean Blog v1.0.0 (http://startbootstrap.com)
+ * Copyright 2015 Start Bootstrap
+ * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
+ */
+
+// Tooltip Init
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
+// responsive tables
+$(document).ready(function() {
+    $("table").each(function(){
+      if ($(this).parent().get(0).tagName != 'FIGURE') {
+        $(this).addClass("table table-responsive table-striped table-hover");
+        $(this).find("th").addClass("text-center");
+      }
     });
+});
 
-    if (typeof (moment) === 'function') {
-        $('.article-meta time').each(function () {
-            $(this).text(moment($(this).attr('datetime')).fromNow());
-        });
+// responsive embed videos
+$(document).ready(function () {
+    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+    $('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+    $('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+    $('img').addClass('img-responsive-center')
+});
+
+// whether a post
+function isPages(attr){
+    var currentBoolean = document.querySelector('.navbar.header-navbar').getAttribute(attr);
+    if(currentBoolean === 'true'){return true;}
+    return false;
+}
+/*
+    scroll function
+    3 parameters
+        1. a DOM object
+        2 a class for targeted object
+        3 height when acctivated (optional. default: the height of the DOM)
+*/
+function scrollCheck(scrollTarget, toggleClass, scrollHeight){
+    document.addEventListener('scroll',function(){
+    var currentTop = window.pageYOffset;
+        currentTop > (scrollHeight||scrollTarget.clientHeight)
+        ?scrollTarget.classList.add(toggleClass)
+        :scrollTarget.classList.remove(toggleClass)
+    })
+}
+
+
+
+/*
+* Steps
+* 1. get the content of h1
+* 2. scroll and appear fixed navbar
+* 3. the content of h1 is shown center at the top of the page
+* */
+
+(function(){
+    if (isPages('data-ispost')){
+        var navbar = document.querySelector('.navbar-custom');
+        var introHeader = document.querySelector('.intro-header').offsetHeight;
+        var introHeader = introHeader > 597 ? introHeader : 500;
+        var toc = document.querySelector('.toc-wrap');
+        scrollCheck(toc,'toc-fixed',introHeader-60);
+        // scrollCheck(navbar,'is-fixed');
     }
-
-    $('.article > .content > table').each(function () {
-        if ($(this).width() > $(this).parent().width()) {
-            $(this).wrap('<div class="table-overflow"></div>');
-        }
-    });
-
-    function adjustNavbar() {
-        const navbarWidth = $('.navbar-main .navbar-start').outerWidth() + $('.navbar-main .navbar-end').outerWidth();
-        if ($(document).outerWidth() < navbarWidth) {
-            $('.navbar-main .navbar-menu').addClass('is-flex-start');
-        } else {
-            $('.navbar-main .navbar-menu').removeClass('is-flex-start');
-        }
-    }
-    adjustNavbar();
-    $(window).resize(adjustNavbar);
-
-    $('figure.highlight table').wrap('<div class="highlight-body">');
-    if (typeof (IcarusThemeSettings) !== 'undefined' &&
-        typeof (IcarusThemeSettings.article) !== 'undefined' &&
-        typeof (IcarusThemeSettings.article.highlight) !== 'undefined') {
-
-        $('figure.highlight').addClass('hljs');
-        $('figure.highlight .code .line span').each(function () {
-            const classes = $(this).attr('class').split(/\s+/);
-            if (classes.length === 1) {
-                $(this).addClass('hljs-' + classes[0]);
-                $(this).removeClass(classes[0]);
-            }
-        });
-
-
-        var clipboard = IcarusThemeSettings.article.highlight.clipboard;
-        var fold = IcarusThemeSettings.article.highlight.fold;
-        fold = fold.trim();
-
-        $('figure.highlight').each(function () {
-            if ($(this).find('figcaption').length) {
-                $(this).find('figcaption').addClass('level is-mobile');
-                $(this).find('figcaption').append('<div class="level-left">');
-                $(this).find('figcaption').append('<div class="level-right">');
-                $(this).find('figcaption div.level-left').append($(this).find('figcaption').find('span'));
-                $(this).find('figcaption div.level-right').append($(this).find('figcaption').find('a'));
-            } else {
-                if (clipboard || fold) {
-                    $(this).prepend('<figcaption class="level is-mobile"><div class="level-left"></div><div class="level-right"></div></figcaption>');
-                }
-            }
-        });
-
-        if (typeof (ClipboardJS) !== 'undefined' && clipboard) {
-            $('figure.highlight').each(function () {
-                var id = 'code-' + Date.now() + (Math.random() * 1000 | 0);
-                var button = '<a href="javascript:;" class="copy" title="Copy" data-clipboard-target="#' + id + ' .code"><i class="fas fa-copy"></i></a>';
-                $(this).attr('id', id);
-                $(this).find('figcaption div.level-right').append(button);
-            });
-            new ClipboardJS('.highlight .copy');
-        }
-
-        if (fold) {
-            var button = '<span class="fold">' + (fold === 'unfolded' ? '<i class="fas fa-angle-down"></i>' : '<i class="fas fa-angle-right"></i>') + '</span>';
-            $('figure.highlight').each(function () {
-                // 此处find ">folded" span,如果有自定义code头,并且">folded"进行处理
-                // 使用示例，.md 文件中头行标记">folded"
-                // ```java main.java >folded
-                // import main.java
-                // private static void main(){
-                //     // test
-                //     int i = 0;
-                //     return i;
-                // }
-                // ```
-                if ($(this).find('figcaption').find('span').length > 0) {
-                    let spanArr = $(this).find('figcaption').find('span');
-                    if (spanArr[0].innerText.indexOf(">folded") > -1) {
-                        // 去掉folded
-                        spanArr[0].innerText = spanArr[0].innerText.replace(">folded", "")
-                        button = '<span class="fold"><i class="fas fa-angle-right"></i></span>';
-                        $(this).find('figcaption div.level-left').prepend(button);
-
-                        // 收叠代码块
-                        toggleFold(this, true);
-                        return;
-                    }
-                }
-                $(this).find('figcaption div.level-left').prepend(button);
-                toggleFold(this, fold === 'folded');
-            });
-
-            function toggleFold(codeBlock, isFolded) {
-                var $toggle = $(codeBlock).find('.fold i');
-                !isFolded ? $(codeBlock).removeClass('folded') : $(codeBlock).addClass('folded');
-                !isFolded ? $toggle.removeClass('fa-angle-right') : $toggle.removeClass('fa-angle-down');
-                !isFolded ? $toggle.addClass('fa-angle-down') : $toggle.addClass('fa-angle-right');
-            }
-
-            // $('figure.highlight').each(function () {
-            //     toggleFold(this, fold === 'folded');
-            // });
-            $('figure.highlight figcaption .fold').click(function () {
-                var $code = $(this).closest('figure.highlight');
-                toggleFold($code.eq(0), !$code.hasClass('folded'));
-            });
-        }
-    }
-
-    var $toc = $('#toc');
-    if ($toc.length > 0) {
-        var $mask = $('<div>');
-        $mask.attr('id', 'toc-mask');
-
-        $('body').append($mask);
-
-        function toggleToc() {
-            $toc.toggleClass('is-active');
-            $mask.toggleClass('is-active');
-        }
-
-        $toc.on('click', toggleToc);
-        $mask.on('click', toggleToc);
-        $('.navbar-main .catalogue').on('click', toggleToc);
-    }
-
-    // hexo-util/lib/is_external_link.js
-    function isExternalLink(input, sitehost, exclude) {
-        try {
-            sitehost = new URL(sitehost).hostname;
-        } catch (e) { }
-
-        if (!sitehost) return false;
-
-        // handle relative url
-        const data = new URL(input, 'http://' + sitehost);
-
-        // handle mailto: javascript: vbscript: and so on
-        if (data.origin === 'null') return false;
-
-        const host = data.hostname;
-
-        if (exclude) {
-            exclude = Array.isArray(exclude) ? exclude : [exclude];
-
-            if (exclude && exclude.length) {
-                for (const i of exclude) {
-                    if (host === i) return false;
-                }
-            }
-        }
-
-        if (host !== sitehost) return true;
-
-        return false;
-    }
-
-    if (typeof (IcarusThemeSettings) !== 'undefined' &&
-        typeof (IcarusThemeSettings.site.url) !== 'undefined' &&
-        typeof (IcarusThemeSettings.site.external_link) !== 'undefined' &&
-        IcarusThemeSettings.site.external_link.enable) {
-        $('.article .content a').filter(function (i, link) {
-            return link.href &&
-                !$(link).attr('href').startsWith('#') &&
-                link.classList.length === 0 &&
-                isExternalLink(link.href,
-                    IcarusThemeSettings.site.url,
-                    IcarusThemeSettings.site.external_link.exclude);
-        }).each(function (i, link) {
-            link.relList.add('noopener');
-            link.target = '_blank';
-        });
-    }
-})(jQuery);
+})();
